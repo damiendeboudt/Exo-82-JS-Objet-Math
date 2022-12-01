8ff2eaee071b81c0345ae5793aa527a38550f64a
Damien <damien.deboudt@sfr.fr>
2022-12-01 10:54:54 +0100

    ok

---
+++ exo.js
@@ -0,0 +1,50 @@
+let  value1 = 34.589456;
+value1 = Math.floor(value1);
+console.log("1 = " + value1);
+
+let value2 = 68.412784;
+value2 = Math.floor(value2);
+console.log("2 = " + value2);
+
+let value3 = 39.456;
+value3 = Math.ceil(value3);
+console.log("3 = " + value3);
+
+let value4 = 27.3497654;
+value4 = Math.ceil(value4);
+console.log("4 = " + value4);
+
+
+let value5 = 5.68;
+value5 = Math.round(value5);
+console.log("5 = " + value5);
+
+let value6 = 5.41;
+value6 = Math.round(value6);
+console.log("6 = " + value6);
+
+let value7 = 6.12457;
+value7 = Math.trunc(value7);
+console.log("7 = " + value7);
+
+let value8 = 9.568124;
+value8 = Math.trunc(value8);
+console.log("8 = " + value8);
+
+let value9 = Math.min( 5, 1, 12, -1, 45);
+console.log("9 = " + value9)
+
+let value10 = Math.min( 9, 12, 16, 44, 3);
+console.log("10 = " +value10)
+
+let value11 = Math.max(5, 1, 12, -1, 45);
+console.log("11 = " + value11)
+
+let value12 = Math.max( 9, 12, 16, 44, 3);
+console.log("12 = " +value12)
+
+let value13 = Math.abs(-6);
+console.log("13 = " + value13);
+
+let value14 = Math.abs(-56);
+console.log("14 = " + value14)
